b63dbae729b0d4930c715d6a5028b09da7235ed9
superkookai <weerawut@live.com>
2025-05-01 15:28:59 +0700

    Initial Commit

---
+++ DigitalAndAnalogClock/DigitalAndAnalogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalAndAnalogClockApp.swift
+//  DigitalAndAnalogClock
+//
+//  Created by Weerawut Chaiyasomboon on 01/05/2568.
+//
+
+import SwiftUI
+
+@main
+struct DigitalAndAnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
